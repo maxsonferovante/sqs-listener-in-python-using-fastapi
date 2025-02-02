3b3f53b8bbc04021c443c0c841978a087e64d275
Maxson Almeida <maxsonferovante@gmail.com>
2025-02-02 20:10:43 -0300

    Atualiza o listener SQS para corrigir a leitura das variáveis de ambiente e melhora o tratamento de mensagens recebidas

--- src/sqs_listener/listen_sqs.py
+++ src/sqs_listener/listen_sqs.py
@@ -7,31 +7,35 @@ from dotenv import load_dotenv
 load_dotenv()
 logger = logging.getLogger(__name__)
 
+logger.setLevel(logging.INFO)
 
 def listen_sqs():
     try:
-        sqs = boto3.client('sqs', region_name=os.getenv['REGION_SQS'])
-        logger.info("Listening to SQS - %s", os.getenv['QUEUE_URL'])
+        logger.info("Listening to SQS - %s", os.getenv('QUEUE_URL'))
+        sqs = boto3.client('sqs',
+            endpoint_url=os.getenv('QUEUE_URL'),
+            region_name= os.getenv('REGION_SQS'),
+            aws_access_key_id= os.getenv('KEY_ACCESS'),
+            aws_secret_access_key=os.getenv('KEY_SECRET'))
         response = sqs.receive_message(
-            QueueUrl=os.getenv['QUEUE_URL'],
+            QueueUrl=os.getenv('QUEUE_URL'),
             AttributeNames=['All'],
             MaxNumberOfMessages=1,
             WaitTimeSeconds=20
         )
-        if "Messages" in response:
-            for message in response['Messages']:
-                body = json.loads(message['Body'])
-                message_receipt_handle = message['ReceiptHandle']
-                
-                logger.info("Message received: %s", body)
-                logger.info("Message Receipt Handle: %s", message_receipt_handle)
-                
-
-                sqs.delete_message(
-                    QueueUrl=os.getenv['QUEUE_URL'],
-                    ReceiptHandle=message_receipt_handle
-                )
-        else:
-            logger.info("No messages in queue")
+        logger.info("Response from SQS: %s", response)
+        if 'Messages' in response:
+            message = response['Messages'][0]
+            receipt_handle = message['ReceiptHandle']
+            body = json.loads(message['Body'])
+            logger.info("Received message: %s", body)
+            sqs.delete_message(
+                QueueUrl=os.getenv('QUEUE_URL'),
+                ReceiptHandle=receipt_handle
+            )        
     except Exception as e:
         logger.error("Error in listening to SQS - %s", e)
+
+
+if __name__ == "__main__":
+    listen_sqs()
